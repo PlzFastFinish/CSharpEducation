63de985647921036849b6e1ed00e2941dc5a840b
Fedor <plzfastfinish33@gmail.com>
2023-06-22 23:33:57 +0400

    Task1 консольное приложение hello world!

---
+++ Task1/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            Console.ReadLine();
+
+        }
+    }
+}
